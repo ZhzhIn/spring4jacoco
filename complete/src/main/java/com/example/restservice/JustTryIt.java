3ed7faa09f4562ece3f1b15a5ec0794a2e4175c0
yindongzi <yinzhenzhi@qtrade.com.cn>
2020-12-28 15:47:53 +0800

    for jacoco test

---
+++ complete/src/main/java/com/example/restservice/JustTryIt.java
@@ -0,0 +1,16 @@
+package com.example.restservice;
+
+/**
+ * 〈test〉
+ *
+ * @author zhzh.yin
+ * @create 2020/12/28
+ */
+public class JustTryIt {
+    private int id;
+    public JustTryIt(){
+    }
+    public JustTryIt(int id){
+        this.id = id;
+    }
+}
