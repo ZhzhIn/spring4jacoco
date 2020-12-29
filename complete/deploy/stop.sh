509ca3211ebec6663bc37d75fad62fff36491815
yindongzi <yinzhenzhi@qtrade.com.cn>
2020-12-29 15:04:51 +0800

    修改shell脚本

--- complete/deploy/stop.sh
+++ complete/deploy/stop.sh
@@ -1,11 +1,13 @@
 # 将应用停止
 #stop.sh
-#!/bin/bash
-echo "Stopping SpringBoot Application"
-pid=`ps -ef | grep rest-service-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}'`
-if [ -n "$pid" ]
-then
-   kill -9 $pid
-fi
+#! /bin/bash
+#echo "Stopping SpringBoot Application"
+kill -9 $(netstat -nlp | grep :8080 | awk '{print $7}' | awk -F"/" '{ print $1 }')
+#pid=`ps -ef | grep rest-service-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}'`
+#print pid
+#if [ -n "$pid" ]
+#then
+#   kill -9 $pid
+#fi
 
 #此处personal-0.0.1-SNAPSHOT.jar根据自己的jar包名称修改
