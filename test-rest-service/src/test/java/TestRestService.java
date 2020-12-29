e91b43f57f845617226c661ddc2ca580d189b1ef
yindongzi <yinzhenzhi@qtrade.com.cn>
2020-12-29 16:24:12 +0800

    增加测试代码

---
+++ test-rest-service/src/test/java/TestRestService.java
@@ -0,0 +1,19 @@
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+/**
+ * 〈测试类〉
+ *
+ * @author zhzh.yin
+ * @create 2020/12/29
+ */
+public class TestRestService {
+    @Test
+    public void testRestService(){
+        given().when()
+                .get("http://127.0.0.1:8082/greeting").then()
+        .statusCode(200);
+    }
+}
