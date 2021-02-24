d0368dbb0b662f19601ae727c2ff94bfa6e35cac
yindo <yindongzi@163.com>
2021-02-24 14:03:52 +0800

    调整测试代码

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -12,12 +12,12 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	/*@GetMapping("/greeting")
+	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 	@GetMapping("/greeting2")
 	public Greeting greeting2(@RequestParam(value = "test", defaultValue = "demo") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	}*/
+	}
 }
